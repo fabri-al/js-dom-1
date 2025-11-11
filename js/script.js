4e83b3ab1b55e79ede618ff50a636c4ef9d482e3
MyUsername <my@email.com>
2025-11-11 12:57:02 +0100

    Updated-code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 
-
-
 function lampadina() {
+
     const bottone = document.getElementById("yellow-lamp");
     bottone.style.display = "block";
-/* document.getElementById("yellow-lamp").addEventListener(lampadina()).src = "img/yellow_lamp.png" */
 
 }
 
