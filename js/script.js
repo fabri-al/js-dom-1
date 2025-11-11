889b59570fa3f4577a61398bd9cb53f0f32938ca
MyUsername <my@email.com>
2025-11-11 12:56:12 +0100

    js-dom-1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+
+function lampadina() {
+    const bottone = document.getElementById("yellow-lamp");
+    bottone.style.display = "block";
+/* document.getElementById("yellow-lamp").addEventListener(lampadina()).src = "img/yellow_lamp.png" */
+
+}
+
